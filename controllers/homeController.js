e8564b1bf9d4334cef5360ac08dd47a11b939796
Tushar-mahi-mac <tusharsandhu3022@gmail.com>
2024-03-31 00:08:05 -0700

    changed login and signup layout

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -4,8 +4,10 @@ const User = require("../models/user");
 
 exports.homePage = asyncHandler(async(req, res, next) =>{
     const allUsers = await User.find({}).exec()
-
-    res.render("home", {users: allUsers, isAuthenticated:  req.isAuthenticated(),  user: req.user?req.user.name: ''} );
+    if(req.isAuthenticated())
+        res.redirect('/posts')
+    else
+        res.render("home", {users: allUsers, isAuthenticated:  req.isAuthenticated(),  user: req.user?req.user.name: ''} );
 })
 
 exports.getLogout = asyncHandler(async(req, res, next) =>{
